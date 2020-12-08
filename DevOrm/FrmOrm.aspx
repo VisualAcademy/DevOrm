e6fcbede0a6918dc464a77b7967b9df159fe420c
RedPlus <redplus@redplus.net>
2020-12-08 19:09:47 +0900

    Add project files.

---
+++ DevOrm/FrmOrm.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" 
+    CodeBehind="FrmOrm.aspx.cs" Inherits="DevOrm.FrmOrm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Dapper를 사용한 데이터 입력과 출력</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h2>데이터 입력</h2>
+        <br />이름: <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+        <br />설명: <asp:TextBox ID="txtDescription" runat="server"></asp:TextBox>
+        <br />주소: <asp:TextBox ID="txtAddress" runat="server"></asp:TextBox>
+        <br />전화: <asp:TextBox ID="txtPhone" runat="server"></asp:TextBox>
+        <br /><asp:Button ID="btnSave" runat="server" Text="저장" 
+            OnClick="btnSave_Click" />
+        <h2>데이터 출력</h2>
+        <asp:GridView ID="lstWarehouses" runat="server"></asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ DevOrm/FrmOrm.aspx.cs
@@ -0,0 +1,72 @@
+﻿using Dapper;
+using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace DevOrm
+{
+    public partial class FrmOrm : System.Web.UI.Page
+    {
+        private SqlConnection con;
+
+        public FrmOrm()
+        {
+            //con = new SqlConnection(@"
+            //    Data Source=(localdb)\MSSQLLocalDB;
+            //    AttachDbFilename=|DataDirectory|DevOrm.mdf;
+            //    Initial Catalog=DevOrm;
+            //    Integrated Security=True;");
+            con = new SqlConnection("server=(localdb)\\mssqllocaldb;database=DevOrm;Trusted_Connection=True;"); 
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == false)
+            {
+                DisplayData();
+            }
+        }
+
+        private void DisplayData()
+        {
+            string sql = @"
+                    Select Id, Name, Description, Address, Phone, Coordinate 
+                    From Warehouses Order By Id Desc";
+
+            var list = con.Query<Warehouse>(sql).ToList();
+
+            lstWarehouses.DataSource = list;
+            lstWarehouses.DataBind();
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            Warehouse model = new Warehouse();
+            model.Name = txtName.Text;
+            model.Description = txtDescription.Text;
+            model.Address = txtAddress.Text;
+            model.Phone = txtPhone.Text;
+            model.Coordinate = String.Empty;
+
+            string sql = @"
+                Insert Into Warehouses 
+                    (Name, Description, Address, Phone, Coordinate) 
+                Values 
+                    (@Name, @Description, @Address, @Phone, @Coordinate);";
+
+            con.Execute(sql, model);
+
+            DisplayData(); 
+        }
+    }
+
+    public class Warehouse
+    {
+        public int Id { get; set; }
+        public string Name  { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Coordinate { get; set; }
+    }
+}
